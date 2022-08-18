363059845cddf8411aa8d5b8dc9c20a44d551087
Navid <yaa110@gmail.com>
2022-08-18 15:55:46 +0430

    Remove Send and Sync bounds from Error

--- src/result.rs
+++ src/result.rs
@@ -1,5 +1,5 @@
 /// Represents a general error type
-pub type Error = Box<dyn std::error::Error + Sync + Send>;
+pub type Error = Box<dyn std::error::Error>;
 
 /// Represents an alias for standard library `Result` with error type of `Box<dyn Error>`.
 pub type Result<T> = std::result::Result<T, Error>;
