1def4586fd7917e099ba6ab9b11bed2106537d3b
Ariel Young <ariel.young@modalai.com>
2023-03-13 16:24:37 -0700

    Added support for setting MAC address of created device

--- src/linux/address.rs
+++ src/linux/address.rs
@@ -7,6 +7,18 @@ pub trait Ipv4AddrExt {
     fn from_address(sock: sockaddr) -> Self;
 }
 
+// Ext suffix is in case a dependency on an external MAC address crate is ever pulled in
+// Also matches above naming this way
+pub trait MACAddrExt {
+    fn to_address(&self) -> sockaddr;
+    fn from_address(sock: sockaddr) -> Self;
+}
+
+pub struct MACAddr {
+    data: [u8; 6],
+}
+
+
 fn hton(octets: [u8; 4]) -> u32 {
     (octets[3] as u32) << 24 | (octets[2] as u32) << 16 | (octets[1] as u32) << 8 | octets[0] as u32
 }
@@ -34,5 +46,24 @@ impl Ipv4AddrExt for Ipv4Addr {
     fn from_address(addr: sockaddr) -> Self {
         let sock: libc::sockaddr_in = unsafe { mem::transmute(addr) };
         ntoh(sock.sin_addr.s_addr).into()
+    }
+}
+
+impl MACAddrExt for MACAddr {
+    fn to_address(&self) -> sockaddr {
+        let mut addr: sockaddr = unsafe { mem::zeroed() };
+        addr.sa_family = libc::ARPHRD_ETHER;
+        for i in 0..6 {
+            addr.sa_data[i] = self.data[i] as libc::c_char;
+        }
+        addr
+    }
+
+    fn from_address(sock: sockaddr) -> Self {
+        let mut mac = Self { data: [0; 6] };
+        for i in 0..6 {
+            mac.data[i] = sock.sa_data[i] as u8;
+        }
+        mac
     }
 }
